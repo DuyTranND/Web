ee406cc03ca1f036dc392efffa4f1ba062f8c913
DuyTranND <156642274+DuyTranND@users.noreply.github.com>
2024-09-10 22:38:09 +0700

    Add files via upload

---
+++ web.cdt/src/main/java/web/dao/IUserDao.java
@@ -0,0 +1,15 @@
+package web.dao;
+
+import java.util.List;
+
+import web.models.UserModel;
+
+public interface IUserDao {
+	
+	List<UserModel> findAll();
+	
+	UserModel findById(int id);
+	
+	void insert(UserModel user);
+	
+}
